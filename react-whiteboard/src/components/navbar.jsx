f2f9fed295d6b33bc4f075aa032e9e403c5253d0
Arpit Nain <arpitnain03@gmail.com>
2026-06-29 22:25:06 +0530

    Basic Navbar done

---
+++ react-whiteboard/src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import "./navbar.css";
+
+function Navbar() {
+    return (
+        <nav>
+            <h2>MyShop</h2>
+            <div>
+                <a href="#">Home</a>
+                <a href="#">Shop</a>
+                <a href="#">Cart</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
