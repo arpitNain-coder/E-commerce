a816f802282fa96114dd366077c06c7bc92d5ce6
Arpit Nain <arpitnain03@gmail.com>
2026-06-29 23:20:56 +0530

    1st part of hero/main page

---
+++ react-whiteboard/src/components/hero.jsx
@@ -0,0 +1,20 @@
+import hero from "../assets/hero.png";
+import "./hero.css";
+
+function Hero() {
+    return (
+        <section className="Hero">
+            <div className="HeroLeft">
+                <p>New Collection</p>
+                <h1>Discover the latest fashion trends.</h1>
+                <p>Premium clothing, accessories</p>
+                <button>Shop Now</button>
+            </div>
+            <div className="HeroRight">
+                <img src={hero} alt="Hero Product" />
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
